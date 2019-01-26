b00d0749544cd6fac2835191647b8d69795c9c0f
Yunseok Choi <yunseokchoi@Yunseokui-MacBook-Pro.local>
2019-01-26 19:44:16 +0900

    Add login and logout

--- partner/views.py
+++ partner/views.py
@@ -1,10 +1,33 @@
+from django.contrib.auth import (
+    authenticate,
+    login as auth_login,
+    logout as auth_logout,
+)
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
     ctx = {}
     return render(request, "index.html", ctx)
+
+def login(request):
+    ctx = {}
+
+    if request.method == "GET":
+        pass
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            return redirect("/partner/")
+        else:
+            ctx.update({"error" : "사용자가 없습니다."})
+
+    return render(request, "login.html", ctx)
 
 def signup(request):
     if request.method == "GET":
@@ -14,7 +37,13 @@ def signup(request):
         email = request.POST.get("email")
         password = request.POST.get("password")
         # print(username, email, password)
+
         user = User.objects.create_user(username, email, password)
+        # Article.objects.creatu(title="", content="")
 
     ctx = {}
     return render(request, "signup.html", ctx)
+
+def logout(request):
+    auth_logout(request)
+    return redirect("/partner/")
